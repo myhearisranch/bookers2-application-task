14610db02561e049e29680bcbd4cabd20b9ed852
myhearisranch <myhearisranch@gmail.com>
2021-12-11 15:56:11 +0000

    ユーザのプロフィール更新が成功したときフラッシュメッセージ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,8 +24,14 @@ class UsersController < ApplicationController
 
   def update
       user = User.find(params[:id])
-      user.update(user_params)
-      redirect_to user_path(user.id)
+
+       #https://pikawaka.com/rails/flashより、フラッシュメッセージを実装
+       #↓フラッシュメッセージを実装する方法
+
+      if user.update(user_params)
+        flash[:notice] = "You have updated user successfully."
+        redirect_to user_path(user.id)
+      end
   end
 
   private
