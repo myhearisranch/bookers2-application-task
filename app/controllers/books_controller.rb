7e076885018acce48e6a22ee13e8e844d0eb23a0
myhearisranch <myhearisranch@gmail.com>
2021-12-12 07:29:29 +0000

    undifi undifined method errors for nillを解決

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ def create
         flash[:notice]="You have updated user successfully."
         redirect_to book_path(@book.id)
     else
-        #↓2うの変数は、indexを表示する為の変数
+        #↓2つの変数は、indexを表示する為の変数
         @books =Book.all
         @user = current_user
         render :index
@@ -25,7 +25,12 @@ end
 
 def index
     @books = Book.all
-    @book_new = Book.new
+
+    #index.html.erb undifined method errors for nill
+    #を解決する為に記述
+    #/bookでindex.html.erbを表示する際に必要
+
+    @book = Book.new
 
     #ログインしている人の情報はcurrent_userで取得
     #current_user.idだとidのみを取得し、エラーになる
@@ -45,6 +50,7 @@ end
 def update
     @book = Book.find(params[:id])
     @book.user_id = current_user.id
+
     #フラッシュメッセージ実装
     if @book.update(book_params)
         flash[:notice] = "You have updated user successfully."
