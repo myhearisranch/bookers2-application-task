ab8c82cd510d719a5ca33c74309a5fe8711b858d
EC2 Default User <ec2-user@ip-172-31-18-171.ec2.internal>
2022-03-23 12:15:53 +0000

    非同期コメント 実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,8 +2,8 @@ class BookCommentsController < ApplicationController
     
     def create
         @book = Book.find(params[:book_id])
-        @comment = current_user.book_comments.new(book_comment_params)
-        @comment.book_id = @book.id
+        comment = current_user.book_comments.new(book_comment_params)
+        comment.book_id = @book.id
         comment.save
         render :create
     end
