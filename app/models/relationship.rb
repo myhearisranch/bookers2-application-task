1c26cdab842da596c5aa05fc73eed1390a5973af
EC2 Default User <ec2-user@ip-172-31-33-1.ec2.internal>
2022-04-02 12:30:01 +0000

    relationshipモデル アソシエーション 作成

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,10 @@
 class Relationship < ApplicationRecord
+
+  # 中間テーブルを用いることで
+  # relationshipとfollowerというテーブルが1対多
+  # relationshipとfollowedというテーブルが多対1という関係にする
+
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+
 end
